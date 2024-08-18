a573dcef920e018f07c18feedc2a54f4b433a541
matheusreis041987 <matheusgithub1987@gmail.com>
2024-08-18 20:03:35 -0300

    correções no códigop do app

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("Mais uma alteração foi realizada")
-<<<<<<< HEAD
+
 
 console.log("enviando para o git")
 
-=======
+
 // alterando algumas coisas
->>>>>>> 3ebb2103116e5d1a17d8bfeecc15286277e4457b
