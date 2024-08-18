3ebb2103116e5d1a17d8bfeecc15286277e4457b
matheusreis041987 <matheusgithub1987@gmail.com>
2024-08-18 19:59:30 -0300

    pequenas alterações

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("Mais uma alteração foi realizada")
+// alterando algumas coisas
