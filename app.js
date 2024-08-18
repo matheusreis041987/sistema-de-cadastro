4d2431e72914a94ad2255b2e0b8c420f1ae74fca
matheusreis041987 <matheusgithub1987@gmail.com>
2024-08-18 20:27:36 -0300

    retornando uma versao anterior

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se conectando ao banco de dados")
-console.log("Mais uma alteração foi realizada")
-
-
-console.log("enviando para o git")
-
-
-// alterando algumas coisas
